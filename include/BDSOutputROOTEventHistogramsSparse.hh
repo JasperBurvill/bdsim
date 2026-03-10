30bfa3aebd3416a461a44197399c21eafff08e9a
jburvill <jasperburvill@gmail.com>
2026-03-10 15:25:19 +0100

    Created a preliminary class to hold and access THnSparse histograms.

--- include/BDSOutputROOTEventHistogramsSparse.hh
+++ include/BDSOutputROOTEventHistogramsSparse.hh
@@ -19,13 +19,54 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef BDSOUTPUTROOTEVENTHISTOGRAMSSPARSE_H
 #define BDSOUTPUTROOTEVENTHISTOGRAMSSPARSE_H
 
+#ifndef __ROOTBUILD__
+#include "globals.hh"
+#endif
+
 #include "TObject.h"
 #include "THnSparse.h"
+
+class THnSparse;
+
+/**
+ * @brief Holder for a set of sparse histograms to be stored.
+ *
+ * @author Jasper Burvill
+ */
 
 class BDSOutputROOTEventHistogramsSparse: public TObject
 {
 public:
+  BDSOutputROOTEventHistogramsSparse() = default;
+  BDSOutputROOTEventHistogramsSparse(const BDSOutputROOTEventHistogramsSparse &h);
+  BDSOutputROOTEventHistogramsSparse operator=(const BDSOutputROOTEventHistogramsSparse&) = delete;
+  BDSOutputROOTEventHistogramsSparse(std::vector<THnSparseD*>& histogramIn);
+  virtual ~BDSOutputROOTEventHistogramsSparse();
+
+  /// Interface function to create a 1D histogram using only standard types.
+  int Create1DHistogramSTD(std::string name, std::string title, int nbins, double xmin, double xmax);
+#ifndef __ROOTBUILD__
+  G4int Create1DHistogram(G4String name, G4String title, G4int nbins, G4double xmin, G4double xmax);
+  G4int Create1DHistogram(G4String name, G4String title, std::vector<double>& edges);
+  void Fill1DHistogram(G4int histoId, G4double value, G4double weight = 1.0);
+#endif
+  /// Flush the contents.
+  virtual void Flush();
+
+  /// Copy (using the TH->Clone) method from another instance.
+  void Fill(const BDSOutputROOTEventHistogramsSparse* rhs);
+
+  /// Copy (without using the TH->Clone) method from another instance. (Quicker).
+  void FillSimple(const BDSOutputROOTEventHistogramsSparse* rhs);
+
+  /// @{ Accessors.
+  std::vector<THnSparseD*>& Get1DHistograms() {return histograms1D;}
+  THnSparseD* Get1DHistogram(int iHisto) const {return histograms1D[iHisto];}
+  /// @}
+
 private:
   std::vector<THnSparseD*> histograms1D;
+
+  ClassDef(BDSOutputROOTEventHistogramsSparse,4)
 };
 #endif
--- src/BDSOutputROOTEventHistogramsSparse.cc
+++ src/BDSOutputROOTEventHistogramsSparse.cc
@@ -17,3 +17,87 @@ You should have received a copy of the GNU General Public License
 along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "BDSOutputROOTEventHistogramsSparse.hh"
+
+#include "THnSparse.h"
+#include "BDSDebug.hh"
+#include "BDSException.hh"
+
+ClassImp(BDSOutputROOTEventHistogramsSparse)
+
+
+BDSOutputROOTEventHistogramsSparse::BDSOutputROOTEventHistogramsSparse(const BDSOutputROOTEventHistogramsSparse& rhs):
+  TObject(rhs)
+{
+  Fill(&rhs);
+}
+
+BDSOutputROOTEventHistogramsSparse::BDSOutputROOTEventHistogramsSparse(std::vector<THnSparseD*>& histogramsIn):
+  histograms1D(histogramsIn)
+{;}
+
+BDSOutputROOTEventHistogramsSparse::~BDSOutputROOTEventHistogramsSparse()
+{
+  // Apparently I need this as memory management is different for THnSparse
+  for (auto h : histograms1D) {delete h;}
+}
+
+void BDSOutputROOTEventHistogramsSparse::FillSimple(const BDSOutputROOTEventHistogramsSparse* rhs)
+{
+  if (!rhs)
+    {return;}
+
+  histograms1D = rhs->histograms1D;
+}
+
+void BDSOutputROOTEventHistogramsSparse::Fill(const BDSOutputROOTEventHistogramsSparse* rhs)
+{
+  if (!rhs) { return; }
+
+  // for each histogram, clone (ie copy) it into this object
+  for (auto h : rhs->histograms1D)
+    {histograms1D.push_back(static_cast<THnSparseD*>(h->Clone()));}
+}
+
+int BDSOutputROOTEventHistogramsSparse::Create1DHistogramSTD(std::string name, std::string title,
+                                                             int nbins, double xmin, double xmax)
+{
+  Int_t bins[1] = {nbins};
+  Double_t mins[1] = {xmin};
+  Double_t maxs[1] = {xmax};
+  histograms1D.push_back(new THnSparseD(name.c_str(), title.c_str(), 1, bins, mins, maxs));
+  return (int)histograms1D.size() - 1;
+}
+
+#ifndef __ROOTBUILD__
+
+G4int BDSOutputROOTEventHistogramsSparse::Create1DHistogram(G4String name, G4String title,
+                                                            G4int nbins, G4double xmin, G4double xmax)
+{
+  Int_t bins[1] = {nbins};
+  Double_t mins[1] = {xmin};
+  Double_t maxs[1] = {xmax};
+  histograms1D.push_back(new THnSparseD(name.c_str(), title.c_str(), 1, bins, mins, maxs));
+  return (G4int)histograms1D.size() - 1;
+}
+
+G4int BDSOutputROOTEventHistogramsSparse::Create1DHistogram(G4String name,
+                                                            G4String title,
+                                                            std::vector<double>& edges)
+{
+  throw BDSException(__METHOD_NAME__, "Construction from edges not currently supported for sparse histograms");
+}
+
+void BDSOutputROOTEventHistogramsSparse::Fill1DHistogram(G4int histoId,
+                                                         G4double value,
+                                                         G4double weight)
+{
+  histograms1D[histoId]->Fill(value, weight);
+}
+
+#endif
+
+void BDSOutputROOTEventHistogramsSparse::Flush()
+{
+  for (auto h : histograms1D)
+    {h->Reset();}
+}
